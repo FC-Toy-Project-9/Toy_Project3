e0bfc5a33c36e45b3e5b8e7137503f0cfbc82c4a
JeongUijeong <jeong275117@gmail.com>
2023-11-10 14:54:45 +0900

    feat: 공통 익셉션 처리 구현

---
+++ src/main/java/com/fc/toy_project3/global/exception/InvalidDateFormatException.java
@@ -0,0 +1,7 @@
+package com.fc.toy_project3.global.exception;
+
+public class InvalidDateFormatException extends RuntimeException {
+    public InvalidDateFormatException(String message){
+        super(message);
+    }
+}
