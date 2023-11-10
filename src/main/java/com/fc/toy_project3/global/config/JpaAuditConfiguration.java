98e780b15724672304803f1aa178888fa66f3013
JeongUijeong <jeong275117@gmail.com>
2023-11-10 16:17:28 +0900

    feat: JPA Auditing 설정 추가

---
+++ src/main/java/com/fc/toy_project3/global/config/JpaAuditConfiguration.java
@@ -0,0 +1,10 @@
+package com.fc.toy_project3.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+class JpaAuditConfiguration {
+
+}
