451eacea93da4de0f5302f2eb3e4f70a5a277bd9
JeongUijeong <jeong275117@gmail.com>
2023-11-10 16:19:43 +0900

    feat: JVM timezone 설정

--- src/main/java/com/fc/toy_project3/ToyProject3Application.java
+++ src/main/java/com/fc/toy_project3/ToyProject3Application.java
@@ -1,5 +1,7 @@
 package com.fc.toy_project3;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +12,8 @@ public class ToyProject3Application {
         SpringApplication.run(ToyProject3Application.class, args);
     }
 
+    @PostConstruct
+    public void setTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
