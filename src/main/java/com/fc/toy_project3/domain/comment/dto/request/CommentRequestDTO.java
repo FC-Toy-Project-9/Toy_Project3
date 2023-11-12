1a04ce37f8f80027cc7f6a25632cfa7d9ebdc756
Yurim Yang <yang980329@likelion.org>
2023-11-12 15:44:09 +0900

    fix: Long type NotEmpty Annotation 불가 이슈로 NotNull Annotation 으로 수정

--- src/main/java/com/fc/toy_project3/domain/comment/dto/request/CommentRequestDTO.java
+++ src/main/java/com/fc/toy_project3/domain/comment/dto/request/CommentRequestDTO.java
@@ -1,6 +1,7 @@
 package com.fc.toy_project3.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CommentRequestDTO {
 
-    @NotEmpty(message = "여행 ID를 입력하세요.")
+    @NotNull(message = "여행 ID를 입력하세요.")
     private Long tripId;
-    @NotEmpty(message = "회원 ID를 입력하세요.")
+    @NotNull(message = "회원 ID를 입력하세요.")
     private Long memberId;
     @NotEmpty(message = "댓글을 입력하세요.")
     private String content;
