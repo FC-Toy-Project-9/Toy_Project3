68a3080251c1d7f18529225829f1bedbc760cdfe
JeongUijeong <jeong275117@gmail.com>
2023-11-13 15:13:08 +0900

    feat: 삭제되지 않은 여정을 조회하기 위한 메서드 추가

--- src/main/java/com/fc/toy_project3/domain/itinerary/repository/ItineraryRepository.java
+++ src/main/java/com/fc/toy_project3/domain/itinerary/repository/ItineraryRepository.java
@@ -1,9 +1,11 @@
 package com.fc.toy_project3.domain.itinerary.repository;
 
 import com.fc.toy_project3.domain.itinerary.entity.Itinerary;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
-
+    Optional<Itinerary> findByIdAndDeletedAt(Long id, LocalDateTime localDateTime);
 }
