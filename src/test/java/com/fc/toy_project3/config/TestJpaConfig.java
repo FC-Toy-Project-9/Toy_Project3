33794ce09840713ae55b1de23f050ebe7397b38e
JeongUijeong <jeong275117@gmail.com>
2023-11-14 15:47:52 +0900

    feat: Test Configuration 설정

---
+++ src/test/java/com/fc/toy_project3/config/TestJpaConfig.java
@@ -0,0 +1,10 @@
+package com.fc.toy_project3.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@TestConfiguration
+public class TestJpaConfig {
+
+}
