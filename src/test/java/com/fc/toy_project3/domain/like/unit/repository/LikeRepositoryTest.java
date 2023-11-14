6862e644be8d4ded29eb9ed6b8ed2b49b31dfbdd
unknown <dldmldlsy@naver.com>
2023-11-14 18:24:24 +0900

    feat: 좋아요 repository 테스트코드 고도화 Mock객체 이용한 테스트가 아닌, 실제 H2 DB에 접근하여 테스트 진행 DataJpaTest 활용 (Config설정) save, findById, findByMemberIdAndTripId, deleteById에 대해 테스트 수행

--- src/test/java/com/fc/toy_project3/domain/like/unit/repository/LikeRepositoryTest.java
+++ src/test/java/com/fc/toy_project3/domain/like/unit/repository/LikeRepositoryTest.java
@@ -1,33 +1,99 @@
 package com.fc.toy_project3.domain.like.unit.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-
+import com.fc.toy_project3.config.TestJpaConfig;
 import com.fc.toy_project3.domain.like.entity.Like;
 import com.fc.toy_project3.domain.like.repository.LikeRepository;
 import com.fc.toy_project3.domain.member.entity.Member;
+import com.fc.toy_project3.domain.member.repository.MemberRepository;
 import com.fc.toy_project3.domain.trip.entity.Trip;
+import com.fc.toy_project3.domain.trip.repository.TripRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-@Transactional
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@Import({TestJpaConfig.class})
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class LikeRepositoryTest {
 
-    @Mock
+    @Autowired
     private LikeRepository likeRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @BeforeEach
+    public void reset() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        likeRepository.deleteAll();
+        memberRepository.deleteAll();
+        tripRepository.deleteAll();
+        entityManager.createNativeQuery("TRUNCATE TABLE like").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE member").executeUpdate();
+        entityManager.createNativeQuery("TRUNCATE TABLE trip").executeUpdate();
+        entityManager
+            .createNativeQuery("ALTER TABLE like ALTER COLUMN `id` RESTART WITH 1")
+            .executeUpdate();
+        entityManager
+            .createNativeQuery("ALTER TABLE member ALTER COLUMN `id` RESTART WITH 1")
+            .executeUpdate();
+        entityManager
+            .createNativeQuery("ALTER TABLE trip ALTER COLUMN `id` RESTART WITH 1")
+            .executeUpdate();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+
     @Nested
-    @DisplayName("findByMemberIdAndTripId()은")
+    @DisplayName("save()는")
+    class Context_save {
+
+        @Test
+        @DisplayName("좋아요 정보를 저장할 수 있다.")
+        void _willSuccess() {
+            //given
+            Member member = Member.builder().id(1L).email("fc123@naver.com").nickname("닉1").password("1234").build();
+            Trip trip = Trip.builder().id(1L).name("제주도 여행").startDate(LocalDate.of(2023, 10, 25))
+                .endDate(LocalDate.of(2023, 10, 26)).isDomestic(true).itineraries(new ArrayList<>())
+                .build();
+            Like like = Like.builder().id(1L).trip(trip).member(member).build();
+
+            //when
+            Like result = likeRepository.save(like);
+
+            //then
+            assertEquals(like.getId(), result.getId());
+            assertEquals(member.getId(), result.getMember().getId());
+            assertEquals(trip.getId(), result.getTrip().getId());
+            verify(likeRepository, times(1)).save(any(Like.class));
+        }
+    }
+    @Nested
+    @DisplayName("findByMemberIdAndTripId()는")
     class Context_findByMemberIdAndTripId {
 
         @Test
@@ -39,14 +105,74 @@ public class LikeRepositoryTest {
                 .endDate(LocalDate.of(2023, 10, 26)).isDomestic(true).itineraries(new ArrayList<>())
                 .build();
             Like like = Like.builder().id(1L).trip(trip).member(member).build();
-            given(likeRepository.findByMemberIdAndTripId(1L, 1l)).willReturn(like);
+
+            memberRepository.save(member);
+            tripRepository.save(trip);
+            likeRepository.save(like);
+
 
             // when
             Like result = likeRepository.findByMemberIdAndTripId(1L, 1L);
 
             //then
-            assertEquals(like, result);
-            verify(likeRepository, times(1)).findByMemberIdAndTripId(1L, 1L);
+            assertNotNull(result.getId());
+            assertThat(result).extracting("likeId", "tripId", "memberId").containsExactly(1L, 1L, 1L);
+        }
+    }
+
+    @Nested
+    @DisplayName("findById()는")
+    class Context_findById {
+
+        @Test
+        @DisplayName("특정 id를 가진 좋아요 정보 Entity를 조회할 수 있다.")
+        void _willSuccess() {
+            //given
+            Long likeId = 1L;
+            Member member = Member.builder().id(1L).email("fc123@naver.com").nickname("닉1").password("1234").build();
+            Trip trip = Trip.builder().id(1L).name("제주도 여행").startDate(LocalDate.of(2023, 10, 25))
+                .endDate(LocalDate.of(2023, 10, 26)).isDomestic(true).itineraries(new ArrayList<>())
+                .build();
+            Like like = Like.builder().id(likeId).trip(trip).member(member).build();
+
+            memberRepository.save(member);
+            tripRepository.save(trip);
+            Like savedLike = likeRepository.save(like);
+
+            //when
+            Optional<Like> result = likeRepository.findById(likeId);
+
+            //then
+            assertNotNull(result);
+            assertEquals(savedLike.getId(), result.get().getId());
+            assertEquals(savedLike.getMember().getId(), result.get().getId());
+            assertEquals(savedLike.getTrip().getId(), result.get().getId());
+        }
+    }
+
+    @Nested
+    @DisplayName("deleteById()는")
+    class Context_deleteById {
+
+        @Test
+        @DisplayName("특정 id를 가진 좋아요 정보를 삭제할 수 있다.")
+        void _willSuccess() {
+            //given
+            Member member = Member.builder().id(1L).email("fc123@naver.com").nickname("닉1").password("1234").build();
+            Trip trip = Trip.builder().id(1L).name("제주도 여행").startDate(LocalDate.of(2023, 10, 25))
+                .endDate(LocalDate.of(2023, 10, 26)).isDomestic(true).itineraries(new ArrayList<>())
+                .build();
+            Like like = Like.builder().id(1L).trip(trip).member(member).build();
+
+            memberRepository.save(member);
+            tripRepository.save(trip);
+            Like savedLike = likeRepository.save(like);
+
+            //when
+            likeRepository.deleteById(1L);
+
+            //then
+            assertFalse(likeRepository.existsById(savedLike.getId()));
         }
     }
 }
