7ece2a76a61885bb8f65f6f6e3d7b0f12f78f569
unknown <dldmldlsy@naver.com>
2023-11-15 14:59:09 +0900

    feat: 좋아요 controller 테스트 mockmvc 활용하여 재구현 및 실패케이스 추가

--- src/test/java/com/fc/toy_project3/domain/like/unit/contoller/LikeRestControllerTest.java
+++ src/test/java/com/fc/toy_project3/domain/like/unit/contoller/LikeRestControllerTest.java
@@ -1,38 +1,38 @@
 package com.fc.toy_project3.domain.like.unit.contoller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fc.toy_project3.domain.like.controller.LikeRestController;
 import com.fc.toy_project3.domain.like.dto.request.LikeRequestDTO;
 import com.fc.toy_project3.domain.like.dto.response.LikeResponseDTO;
 import com.fc.toy_project3.domain.like.service.LikeService;
-import com.fc.toy_project3.global.common.ResponseDTO;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(LikeRestController.class)
 public class LikeRestControllerTest {
 
-    @InjectMocks
-    private LikeRestController likeRestController;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private LikeService likeService;
-
-    @BeforeEach
-    void setup() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Nested
     @DisplayName("createLike()는")
@@ -40,7 +40,7 @@ public class LikeRestControllerTest {
 
         @Test
         @DisplayName("좋아요 정보를 등록할 수 있다.")
-        void _willSuccess() {
+        void _willSuccess() throws Exception {
             //given
             Long memberId = 1L;
             Long tripId = 1L;
@@ -50,15 +50,37 @@ public class LikeRestControllerTest {
 
             given(likeService.createLike(memberId, likeRequestDTO)).willReturn(likeResponseDTO);
 
-            //when
-            ResponseEntity<ResponseDTO<LikeResponseDTO>> responseEntity = likeRestController.createLike(likeRequestDTO);
+            //when, then
+            mockMvc.perform(post("/api/likes")
+                .content(new ObjectMapper().writeValueAsString(likeRequestDTO))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.code").exists()).andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.data").isMap())
+                .andExpect(jsonPath("$.data.likeId").exists())
+                .andExpect(jsonPath("$.data.memberId").exists())
+                .andExpect(jsonPath("$.data.tripId").exists())
+                .andDo(print());
+            verify(likeService, times(1)).createLike(any(Long.TYPE), any(LikeRequestDTO.class));
+        }
 
-            //then
-            assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-            assertNotNull(responseEntity.getBody().getData());
-            assertEquals(memberId, responseEntity.getBody().getData().getMemberId());
-            assertEquals(likeRequestDTO.getTripId(), responseEntity.getBody().getData().getTripId());
-            verify(likeService, times(1)).createLike(memberId, likeRequestDTO);
+        @Nested
+        @DisplayName("tripId가 ")
+        class Element_tripId {
+
+            @Test
+            @DisplayName("null일 경우 좋아요 정보를 저장할 수 없다.")
+            void null_willFail() throws Exception {
+                // given
+                LikeRequestDTO likeRequestDTO = LikeRequestDTO.builder().tripId(null).build();
+
+                // when, then
+                mockMvc.perform(post("/api/trip")
+                        .content(new ObjectMapper().writeValueAsString(likeRequestDTO))
+                        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isBadRequest())
+                    .andDo(print());
+                verify(likeService, never()).createLike(any(Long.TYPE), any(LikeRequestDTO.class));
+            }
         }
     }
 
@@ -68,7 +90,7 @@ public class LikeRestControllerTest {
 
         @Test
         @DisplayName("특정 회원 id와 여행 id를 가진 좋아요 정보를 조회할 수 있다.")
-        void _willSuccess() {
+        void _willSuccess() throws Exception {
             //given
             Long memberId = 1L;
             Long tripId = 1L;
@@ -77,14 +99,15 @@ public class LikeRestControllerTest {
 
             given(likeService.getLikeByMemberIdAndTripId(memberId, tripId)).willReturn(likeResponseDTO);
 
-            //when
-            ResponseEntity<ResponseDTO<LikeResponseDTO>> responseEntity = likeRestController.getLikeByMemberIdAndTripId(tripId);
-
-            //then
-            assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-            assertNotNull(responseEntity.getBody().getData());
-            assertEquals(memberId, responseEntity.getBody().getData().getMemberId());
-            assertEquals(tripId, responseEntity.getBody().getData().getTripId());
+            //when, then
+            mockMvc.perform(get("/api/likes/{tripId}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").exists()).andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.data").isMap())
+                .andExpect(jsonPath("$.data.likeId").exists())
+                .andExpect(jsonPath("$.data.memberId").exists())
+                .andExpect(jsonPath("$.data.tripId").exists())
+                .andDo(print());
             verify(likeService, times(1)).getLikeByMemberIdAndTripId(any(Long.TYPE), any(Long.TYPE));
         }
     }
@@ -95,7 +118,7 @@ public class LikeRestControllerTest {
 
         @Test
         @DisplayName("특정 id를 가진 좋아요 정보를 삭제할 수 있다.")
-        void _willSuccess() {
+        void _willSuccess() throws Exception {
             //given
             Long likeId = 1L;
             Long memberId = 1L;
@@ -104,14 +127,15 @@ public class LikeRestControllerTest {
 
             given(likeService.deleteLikeById(memberId, tripId)).willReturn(likeResponseDTO);
 
-            //when
-            ResponseEntity<ResponseDTO<LikeResponseDTO>> responseEntity = likeRestController.deleteLikeById(likeId);
-
-            //then
-            assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-            assertNotNull(responseEntity.getBody().getData());
-            assertEquals(likeId, responseEntity.getBody().getData().getLikeId());
-            assertEquals(memberId, responseEntity.getBody().getData().getMemberId());
+            //when, then
+            mockMvc.perform(delete("/api/likes/{likeId}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").exists()).andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.data").isMap())
+                .andExpect(jsonPath("$.data.likeId").exists())
+                .andExpect(jsonPath("$.data.memberId").exists())
+                .andExpect(jsonPath("$.data.tripId").exists())
+                .andDo(print());
             verify(likeService, times(1)).deleteLikeById(any(Long.TYPE), any(Long.TYPE));
         }
     }
