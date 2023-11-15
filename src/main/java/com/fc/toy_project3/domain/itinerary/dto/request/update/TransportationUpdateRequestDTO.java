a163c3d567028898be300230bda93b4ebeae0c3b
JeongUijeong <jeong275117@gmail.com>
2023-11-15 23:56:03 +0900

    refactor: 여정 생성 및 수정 요청 DTO Validation 추가

--- src/main/java/com/fc/toy_project3/domain/itinerary/dto/request/update/TransportationUpdateRequestDTO.java
+++ src/main/java/com/fc/toy_project3/domain/itinerary/dto/request/update/TransportationUpdateRequestDTO.java
@@ -1,6 +1,7 @@
 package com.fc.toy_project3.domain.itinerary.dto.request.update;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,17 @@ public class TransportationUpdateRequestDTO {
 
     @NotNull(message = "여정 ID를 입력하세요.")
     private Long itineraryId;
+    @Size(max = 100, message = "여정 이름은 100자 이내로 입력하세요.")
     private String itineraryName;
+    @Size(max = 100, message = "이동 수단은 100자 이내로 입력하세요.")
     private String transportation;
+    @Size(max = 100, message = "출발지는 100자 이내로 입력하세요.")
     private String departurePlace;
+    @Size(max = 255, message = "출발지 도로명 주소은 100자 이내로 입력하세요.")
     private String departurePlaceRoadAddressName;
+    @Size(max = 100, message = "도착지는 100자 이내로 입력하세요.")
     private String destination;
+    @Size(max = 255, message = "도착지 도로명 주소는 100자 이내로 입력하세요.")
     private String destinationRoadAddressName;
     private String departureTime;
     private String arrivalTime;
